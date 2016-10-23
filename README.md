dedcf35797a59722b7aebe445a96e9b542e8b968
James Hoekzema <jphx48@mst.edu>
2016-10-23 07:43:00 -0500

    small style update, readme update, and added ability to make and confirm purchases

--- README.md
+++ README.md
@@ -2,21 +2,32 @@
 ## Become a Resource
 
 ## About
-==========
+
 Involved is an application aimed at connecting individuals to the
 problem of homelessness. By signing up, you can begin to offer
 help through the form of openning your home, donating food, clothes,
 and toys to local events, and volunteering.
 
 ## Why Get Involved?
-===========
+
 The more you help, the more local coupons, deals, and prizes become
 available to you! When you donate your time, energy, and resources,
 we reward you with points that can be used to purchase coupons
 provided by businesses and organizations. It's a win-win!
 
 ## How do I Signup?
-===========
+
 Register here. In order to protect the individuals and organizations
 involved, we require you sign up with a verified email address and
 visit your local center in order to get started.
+
+### How to Run
+Make sure to have Golang installed as well as MySQL and the necessary driver.
+
+Before starting the server, you should run the `create.sql` file to create the database.
+
+To run the server, just run the main file.
+
+```
+$ go run main.go
+```
--- api/purchase.go
+++ api/purchase.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"time"
 	"encoding/json"
+	"strconv"
+	"errors"
 )
 
 type Purchase struct{
@@ -21,12 +23,81 @@ func purchaseRoute(w http.ResponseWriter, r *http.Request, user *User) {
 		return
 	}
 
-	rows, err := db.Query("SELECT * FROM purchase p, item i WHERE p.item_id=i.item_id and p.user_id=?", user.Id)
+	switch r.Method {
+	case "GET":
+		purchases, err := getPurchases(user.Id)
+
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, `{"error": "something went wrong"}`, err)
+			return
+		}
+
+		jsonPurchase, err := json.Marshal(purchases)
+
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, `{"error": "something went wrong"}`)
+			return
+		}
+
+		fmt.Fprint(w, string(jsonPurchase))
+	case "POST":
+		item, ok := r.URL.Query()["item"]
+
+		if !ok {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, `{"error": "must provide item identifier"}`)
+			return
+		}
+
+		itemId, _ := strconv.ParseInt(item[0], 10, 0)
+
+		err := makePurchase(user.Id, int(itemId))
+
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, `{"error": "something went wrong"}`)
+			return
+		}
+
+		fmt.Fprintf(w, `{"msg": "success"}`)
+	case "PUT":
+		if user.Type == "host" || user.Type == "seeker" {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, `{"error": "you are not authorized to confirm purchases"}`)
+			return
+		}
+
+		itemStr, iok := r.URL.Query()["item"]
+		buyerStr, bok := r.URL.Query()["buyer"]
+
+		if !iok || !bok {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, `{"error": "must provide both a item identifier and buyer identifier"}`)
+			return
+		}
+
+		item, _ := strconv.ParseInt(itemStr[0], 10, 0)
+		buyer, _ := strconv.ParseInt(buyerStr[0], 10, 0)
+
+		err := confirmPurchase(int(buyer), user.Id, int(item))
+
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, `{"error": "could not confirm purchase"}`, err)
+			return
+		}
+
+		fmt.Fprintf(w, `{"msg": "success"}`)
+	}
+}
+
+func getPurchases(id int) ([]*Purchase, error) {
+	rows, err := db.Query("SELECT * FROM purchase p, item i WHERE p.item_id=i.item_id and p.user_id=?", id)
 
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, `{"error": "something went wrong"}`, err)
-		return
+		return nil, err
 	}
 
 	defer rows.Close()
@@ -49,18 +120,35 @@ func purchaseRoute(w http.ResponseWriter, r *http.Request, user *User) {
 	}
 
 	if rows.Err() != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, `{"error": "something went wrong"}`)
-		return
+		return nil, err
 	}
 
-	jsonPurchase, err := json.Marshal(purchases)
+	return purchases, nil
+}
+
+func makePurchase(user, item int) error {
+	_, err := db.Exec("INSERT INTO purchase (user_id, item_id) VALUES (?, ?)", user, item)
+
+	return err
+}
+
+func confirmPurchase(buyer, seller, item int) error {
+	res, err := db.Exec("UPDATE purchase p, item i SET fulfilled=1 " +
+		"WHERE p.user_id=? and p.item_id=? and p.item_id=i.item_id and i.seller=?",
+		buyer,
+		item,
+		seller,
+	)
 
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, `{"error": "something went wrong"}`)
-		return
+		return err
 	}
 
-	fmt.Fprint(w, string(jsonPurchase))
+	rows, err := res.RowsAffected()
+
+	if err != nil || rows == 0 {
+		return errors.New("could not confirm purchase")
+	}
+
+	return nil
 }
